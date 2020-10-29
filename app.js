ec36278a63500d985e39572c5399470bc7c4caba
Nick Cave <nxdmedia@googlemail.com>
2020-10-29 12:42:46 +0000

    Syncing database everytime server starts

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 const db = require('./models/index');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -21,35 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// Test connection to the datbase
-(async () => {
-  try {
-    await db.sequelize.authenticate();
-    console.log('Connection to the database successful!');
-  } catch(error) {
-    console.error('Error connecting to the database: ', error);
-  }
-})();
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-module.exports = app;
+// app.use('/users', usersRouter);
 
 // Catching 404 error
 app.use((req, res, next) => {
@@ -61,7 +33,6 @@ app.use((req, res, next) => {
 
 // Error handler
 app.use((err, req, res, next) => {
-  // res.locals.error = err;
   if (err.status === 404) {
     res.status(404)
     res.render('page-not-found', {err});
@@ -71,3 +42,6 @@ app.use((err, req, res, next) => {
     res.render('error', {err})
   }
 });
+
+module.exports = app;
+
